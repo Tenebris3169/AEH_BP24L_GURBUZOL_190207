bbe6434ccc15e3c95b262e10c040cff08034b2d4
Tenebris3169 <koraygurbuzol@hotmail.com>
2025-03-26 15:20:16 +0100

    First part of TASK2

--- src/pl/pp/MyThirdApp.java
+++ src/pl/pp/MyThirdApp.java
@@ -2,58 +2,31 @@ package pl.pp;
 import java.util.Scanner;
 public class MyThirdApp {
 
-    public static void main(String[] args){
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+                while (true) {
 
-        System.out.println("Enter a number greater than 100:");
+                    System.out.print("Enter the number of days (enter a non-positive number to exit): ");
+                    int days = scanner.nextInt();
 
-        //WHILE
-        var number1 = scanner.nextDouble();
-        while (number1 <= 100 ){
-            System.out.println("The number is not greater than 100, enter again");
-            number1 = scanner.nextDouble();
-        }
-        System.out.println("Thank you! You've entered: " + number1);
 
-        //DO
-        double number2;
-        do {
-            System.out.println("Enter a number greater than 200:");
-            number2 = scanner.nextDouble();
-        } while (number2 <=200);
-        System.out.println("Thank you! You've entered: " + number2);
+                    if (days <= 0) {
+                        System.out.println("Exiting program. Goodbye!");
+                        break;
+                    }
 
-        //FOR
-        int wynik = 0;
-        for (var i = 1; i<= 10; i++){
-            wynik = wynik + i;
-            System.out.println("Iteration no. " + i + "in the for loop, and the result (wynik) = " + wynik);
-        //IF ELSE
-            System.out.println("Enter the value of x: ");
-            var x = scanner.nextDouble();
+                    // Convert days to weeks and remaining days
+                    int weeks = days / 7;
+                    int remainingDays = days % 7;
 
-            System.out.println("Enter the value of y: ");
-            var y = scanner.nextDouble();
-
-            if(x > y){
-                System.out.println("x is greater than y");
-            } else if (x < y){
-                System.out.println("x is smaller than y");
-            } else {
-                System.out.println("x equals y");
-            }
-
-            //terminating the program by pressing a key
-            while (true){
-                System.out.println("Enter -1 to exit the program");
-                var input = scanner.nextDouble();
-                if(input== -1){
-                    System.out.println("Exit...");
-                    break;
+                    // Display result
+                    System.out.println(days + " days is " + weeks + " weeks and " + remainingDays + " days.");
                 }
+
+                scanner.close();
             }
-            scanner.close();
         }
-    }
-}
+
+
+
