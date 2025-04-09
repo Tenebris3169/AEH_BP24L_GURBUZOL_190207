2bc82d69fc536722b41a55ce832d45126c3defa9
Tenebris3169 <koraygurbuzol@hotmail.com>
2025-04-09 14:23:44 +0200

    Second Task

--- src/pl/pp/MyFifthApp.java
+++ src/pl/pp/MyFifthApp.java
@@ -1,53 +1,78 @@
 package pl.pp;
+
 import java.util.Scanner;
+
 public class MyFifthApp {
 
-            public static void main(String[] args) {
-                System.out.println("Calculations and display of the result for values assigned in the application code");
-                boolean gameOver = true;
-                int score = 800;
-                int levelCompleted = 5;
-                int bonus = 100;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-                int finalScore = score;
-                if (gameOver) {
-                    finalScore += (levelCompleted * bonus);
-                    finalScore += 1000;
-                    System.out.println("Your score #1 is " + finalScore);
-                }
+        System.out.print("Enter a character to display: ");
+        char character = scanner.next().charAt(0);
 
-                // Calculations and display of the result for values assigned in the application code (different than before)
-                System.out.println("Calculations and display of the result for values assigned in the application code (different than before)");
-                score = 10000;
-                levelCompleted = 8;
-                bonus = 200;
+        System.out.print("Enter how many times the character should be printed per line: ");
+        int timesPerLine = scanner.nextInt();
 
-                finalScore = score;
+        System.out.print("Enter how many lines should be printed: ");
+        int numberOfLines = scanner.nextInt();
 
-                if (gameOver) {
-                    finalScore += (levelCompleted * bonus);
-                    finalScore += 1000;
-                    System.out.println("Your score #2 is " + finalScore);
-                }
+        printCharacterGrid(character, timesPerLine, numberOfLines);
 
-                // Calculations using the method calculateScore_noArguments(), but still for values assigned in the method code
-                System.out.println("Calculations using the method calculateScore_noArguments(), but still for values assigned in the method code");
-                calculateScore_noArguments();
+        scanner.close();
 
-                // Calculations using the method calculateScore(), but this time for values entered into the method call argument
-                System.out.println("Calculations using the method calculateScore(), but this time for values entered into the method call argument");
-                calculateScore_arguments(true, 2500, 9, 250);
-                // it can be called using previously declared variables
-                calculateScore_arguments(gameOver, score, levelCompleted, bonus);
-                // but as you see, the display function is also with number #4 (because such display is recorded
-                // in the method. What to do to avoid this? Below:
+        // === ORIGINAL CODE (Commented Out) ===
+/*
+        System.out.println("Calculations and display of the result for values assigned in the application code");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-                // Calculations and display can be made independent from each other by adding a parameter that this method will return using return
-                System.out.println("Calculations and display can be made independent from each other by adding a parameter that this method will return using return");
-                finalScore = calculateScore_argumentsReturn(gameOver, score, levelCompleted, bonus);
-                System.out.println("Your score #5 is " + finalScore);
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #1 is " + finalScore);
+        }
+
+        System.out.println("Calculations and display of the result for values assigned in the application code (different than before)");
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #2 is " + finalScore);
+        }
+
+        System.out.println("Calculations using the method calculateScore_noArguments(), but still for values assigned in the method code");
+        calculateScore_noArguments();
+
+        System.out.println("Calculations using the method calculateScore(), but this time for values entered into the method call argument");
+        calculateScore_arguments(true, 2500, 9, 250);
+        calculateScore_arguments(gameOver, score, levelCompleted, bonus);
+
+        System.out.println("Calculations and display can be made independent from each other by adding a parameter that this method will return using return");
+        finalScore = calculateScore_argumentsReturn(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your score #5 is " + finalScore);
+*/
+    }
+
+    // === NEW METHOD ===
+    private static void printCharacterGrid(char ch, int timesPerLine, int numberOfLines) {
+        for (int i = 0; i < numberOfLines; i++) {
+            for (int j = 0; j < timesPerLine; j++) {
+                System.out.print(ch);
             }
+            System.out.println();
+        }
+    }
 
+    // === ORIGINAL METHODS (Commented Out) ===
+/*
     private static void calculateScore_noArguments() {
         boolean gameOver = true;
         int score = 150;
@@ -61,6 +86,7 @@ public class MyFifthApp {
             System.out.println("Your score #3 is " + finalScore);
         }
     }
+
     private static void calculateScore_arguments(boolean gameOver, int score, int levelCompleted, int bonus) {
         int finalScore = score;
         if (gameOver) {
@@ -69,6 +95,7 @@ public class MyFifthApp {
             System.out.println("Your score #4 is " + finalScore);
         }
     }
+
     private static int calculateScore_argumentsReturn(boolean gameOver, int score, int levelCompleted, int bonus) {
         int finalScore = score;
         if (gameOver) {
@@ -76,8 +103,6 @@ public class MyFifthApp {
             finalScore += 1000;
         }
         return finalScore;
-            }
-        }
-
-
-
+    }
+*/
+}
